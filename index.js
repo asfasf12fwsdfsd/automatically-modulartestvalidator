49a6dcd9c9aa785cc978c1f0b2e07ec6c4421082
asfasf12fwsdfsd <js1mecnun36@hotmail.com>
2024-05-07 23:49:26 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const quickSortRandomPivot = (arr) => {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  const pivotIndex = Math.floor(Math.random() * arr.length);
-  const pivot = arr[pivotIndex];
-  const left = [];
-  const right = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== pivotIndex) {
-      if (arr[i] <= pivot) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
-    }
-  }
-  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
-};
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log(isPalindrome("racecar")); // true
