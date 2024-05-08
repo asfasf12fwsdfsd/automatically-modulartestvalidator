105da261dbc1732496ad7124b115abc3aa976679
asfasf12fwsdfsd <js1mecnun36@hotmail.com>
2024-05-08 20:59:36 +0700

    First commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
+function isValidBST(root) {
+  return isValid(root, null, null);
 }
-
-console.log(isPalindrome("racecar")); // true
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
+}
