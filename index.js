bdd2aecfae558ea08a43df225aada59ae504e984
asfasf12fwsdfsd <js1mecnun36@hotmail.com>
2024-05-10 00:53:55 +0700

    update project

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-}
-function isValid(node, min, max) {
-  if (!node) return true;
-  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-    return false;
-  return (
-    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-  );
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(current) {
+    if (current.length === nums.length) {
+      result.push([...current]);
+      return;
+    }
+    for (const num of nums) {
+      if (current.includes(num)) continue;
+      current.push(num);
+      backtrack(current);
+      current.pop();
+    }
+  }
 }
